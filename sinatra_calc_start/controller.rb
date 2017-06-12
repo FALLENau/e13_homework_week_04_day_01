70e4843103549f7c3d3298d263051f9897903120
FALLENau <reece.c.jones@gmail.com>
2017-06-12 16:05:53 +0100

    css.style

--- sinatra_calc_start/controller.rb
+++ sinatra_calc_start/controller.rb
@@ -5,25 +5,36 @@ require "json"
 require_relative "models/calculator"
 
 
+get '/' do
+  erb(:home)
+end
+
+get '/about_us' do
+  erb(:about_us)
+end
+
 get '/add/:number1/:number2' do
   calculator = Calculator.new(params["number1"].to_i, params["number2"].to_i)
   @calculation = calculator.add()
-  erb(:result)#note: erb takes in syimble and looks for a erb file by the name of :result.
+  erb(:result)#note: erb takes in syimble and looks for a erb file by the name of result.
 end #note: erb in rb file expexts :syimboles.
 
 get '/subtract/:number1/:number2' do
   calculator = Calculator.new(params["number1"].to_i, params["number2"].to_i)
-  return "#{calculator.subtract()}"
+  @calculation = calculator.subtract()
+  erb(:result)
 end
 
 get '/multiply/:number1/:number2' do
   calculator = Calculator.new(params["number1"].to_i, params["number2"].to_i)
-  return "#{calculator.multiply()}"
+  @calculation = calculator.multiply()
+  erb(:result)
 end
 
 get '/divide/:number1/:number2' do
   calculator = Calculator.new(params["number1"].to_f, params["number2"].to_f)
-  return "#{calculator.divide()}"
+  @calculation = calculator.divide()
+  erb(:result)
 end
 
 
